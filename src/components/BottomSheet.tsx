1c071c615f350bbf51a351becf838018f02eb187
shuttlekim <kunkun7762@gmail.com>
2024-10-10 13:17:56 +0900

    feat: update bottom sheet

--- src/components/BottomSheet.tsx
+++ src/components/BottomSheet.tsx
@@ -1,4 +1,4 @@
-import React, {useRef, useEffect, ReactNode, Key} from 'react';
+import React, {useRef, useEffect, Key} from 'react';
 import {
   View,
   Text,
@@ -7,15 +7,21 @@ import {
   Dimensions,
   PanResponder,
   TouchableOpacity,
+  TouchableWithoutFeedback,
 } from 'react-native';
+import {NavigationProp, useNavigation} from '@react-navigation/native';
 import useBottomSheetStore from '../store/useBottomSheetStore';
 
-const { height: screenHeight } = Dimensions.get('window');
+interface INav extends NavigationProp<any> {}
+
+const {height: screenHeight} = Dimensions.get('window');
 
 const BottomSheet = () => {
   const isOpen = useBottomSheetStore(state => state.isOpen);
   const menuItems = useBottomSheetStore(state => state.menuItems);
   const closeBottomSheet = useBottomSheetStore(state => state.closeBottomSheet);
+
+  const navigation = useNavigation<INav>();
 
   const translateY = useRef(new Animated.Value(screenHeight)).current;
 
@@ -25,10 +31,12 @@ const BottomSheet = () => {
         return gestureState.dy > 0;
       },
       onPanResponderMove: (_, gestureState) => {
-        translateY.setValue(gestureState.dy);
+        if (gestureState.dy > 0) {
+          translateY.setValue(gestureState.dy);
+        }
       },
       onPanResponderRelease: (_, gestureState) => {
-        if (gestureState.dy > screenHeight / 4) {
+        if (gestureState.dy > screenHeight / 8) {
           closeBottomSheetWithAnimation();
         } else {
           Animated.timing(translateY, {
@@ -56,7 +64,7 @@ const BottomSheet = () => {
       useNativeDriver: true,
     }).start(() => closeBottomSheet());
   };
-  
+
   useEffect(() => {
     if (isOpen) {
       openBottomSheet();
@@ -65,43 +73,60 @@ const BottomSheet = () => {
     }
   }, [isOpen]);
 
-  return (
-    <Animated.View
-      style={[styles.bottomSheetContainer, { transform: [{ translateY }] }]}
-      {...panResponder.panHandlers} // 드래그 제스처 핸들러
-    >
-      <View style={styles.header}>
-        <Text style={styles.headerText}>Drag down to close</Text>
-      </View>
-      <View style={styles.contentContainer}>
-        {menuItems.map((
-          item: {
-            icon: string,
-            title: string,
-            link: string
-          }, 
-          index: Key) => (
-          <TouchableOpacity
-            key={index}
-            style={styles.menuItem}
-            onPress={() => console.log(`${item} clicked`)}
-          >
-            <Text style={styles.menuText}>{item.title}</Text>
-          </TouchableOpacity>
-        ))}
-      </View>
-    </Animated.View>
-  );
+  return isOpen ? (
+    <View style={styles.overlay}>
+      <TouchableWithoutFeedback onPress={closeBottomSheetWithAnimation}>
+        <View style={styles.background} />
+      </TouchableWithoutFeedback>
+      <Animated.View
+        style={[styles.bottomSheetContainer, { transform: [{ translateY }] }]}
+        {...panResponder.panHandlers} // 드래그 제스처 핸들러
+      >
+        <View style={styles.contentContainer}>
+          {menuItems.map((
+            item: {
+              icon: string,
+              title: string,
+              link: string
+            }, 
+            index: Key) => (
+            <TouchableOpacity
+              key={index}
+              style={styles.menuItem}
+              onPress={() => {
+                navigation.navigate(item.link);
+                closeBottomSheetWithAnimation();
+              }}
+            >
+              <Text style={styles.menuText}>{item.title}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+      </Animated.View>
+    </View>
+  ) : null;
 };
 
 const styles = StyleSheet.create({
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
+  },
+  background: {
+    flex: 1,
+    backgroundColor: 'rgba(1, 1, 1, 0.5)', // 투명한 배경
+  },
   bottomSheetContainer: {
     position: 'absolute',
     left: 0,
     right: 0,
     bottom: 0,
     height: screenHeight / 4,
-    backgroundColor: '#ffffff',
+    backgroundColor: '#35353C',
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
     shadowColor: '#000',
@@ -127,12 +152,11 @@ const styles = StyleSheet.create({
   },
   menuItem: {
     paddingVertical: 12,
-    borderBottomWidth: 1,
-    borderBottomColor: '#eaeaea',
   },
   menuText: {
     fontSize: 18,
     fontWeight: '500',
+    color: '#fff',
   },
 });
 
