1bb8fcf4961aa88adf43b8839eb0efe7da3c4a6e
shuttlekim <kunkun7762@gmail.com>
2024-10-29 19:02:39 +0900

    feat: fix

--- src/bottomTabs/HomeTab.tsx
+++ src/bottomTabs/HomeTab.tsx
@@ -1,17 +1,19 @@
 import React, {useLayoutEffect} from 'react';
-import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
+import {getFocusedRouteNameFromRoute} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomeMain from '../screens/home/HomeMain';
 import MakeCard from '../screens/home/MakeCard';
 import RegisterCard from '../screens/home/RegisterCard';
 import CardDetail from '../screens/home/CardDetail';
-import useTabBarVisibilityStore from '../store/useTabBarVisibilityStore'; 
+import useTabBarVisibilityStore from '../store/useTabBarVisibilityStore';
+import {TouchableOpacity} from 'react-native';
+
+const BackIcon = require('../assets/icons/BackIcon.svg').default;
 
 const Stack = createStackNavigator();
 
-const HomeTab: React.FC<{ route: any }> = ({ route }) => {
-
-  const { showTabBar, hideTabBar } = useTabBarVisibilityStore();
+const HomeTab: React.FC<{route: any}> = ({route}) => {
+  const {showTabBar, hideTabBar} = useTabBarVisibilityStore();
 
   useLayoutEffect(() => {
     const routeName = getFocusedRouteNameFromRoute(route);
@@ -26,19 +28,29 @@ const HomeTab: React.FC<{ route: any }> = ({ route }) => {
       <Stack.Screen
         name="HomeMain"
         component={HomeMain}
-        options={{ headerShown: false }}
+        options={{headerShown: false}}
       />
+<<<<<<< Updated upstream
       <Stack.Screen
         name="CardDetail"
         component={CardDetail}
       />
+=======
+      {/* <Stack.Screen
+        name="MakeCard"
+        component={MakeCard}
+      /> */}
+      <Stack.Screen name="CardDetail" component={CardDetail} />
+>>>>>>> Stashed changes
       <Stack.Screen
         name="MakeCard"
         component={MakeCard}
+        options={{headerShown: false}}
       />
       <Stack.Screen
         name="RegisterCard"
         component={RegisterCard}
+        options={{headerShown: false}}
       />
     </Stack.Navigator>
   );
--- src/screens/home/RegisterCard.tsx
+++ src/screens/home/RegisterCard.tsx
@@ -1,12 +1,44 @@
-import React from "react";
-import { Button, Text, View } from "react-native";
+import React from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+} from 'react-native';
+
+const BackIcon = require('../../assets/icons/BackIcon.svg').default;
 
 const RegisterCard: React.FC = () => {
   return (
-    <View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity style={styles.leftIcon}>
+          <BackIcon />
+        </TouchableOpacity>
+      </View>
       <Text>RegisterCard</Text>
-    </View>
+    </SafeAreaView>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#282828',
+  },
+  header: {
+    flex: 0.1,
+    textAlign: 'center',
+    backgroundColor: '#fff',
+    justifyContent: 'space-between',
+  },
+  leftIcon: {
+    paddingLeft: 16,
+    flex: 1,
+    justifyContent: 'center',
+    color: '#fff',
+  },
+});
 
 export default RegisterCard;
