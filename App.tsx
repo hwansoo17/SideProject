30b6bf06b9dfc5248f8fee0eaba70f2f88f9c38e
shuttlekim <kunkun7762@gmail.com>
2024-10-29 17:59:41 +0900

    feat: update deep link

--- App.tsx
+++ App.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import RootNavigator from './src/Nav/RootNavigator';
+import { Linking } from 'react-native';
 const queryClient = new QueryClient();
 
 const App = () => {
+  useEffect(() => {
+    const getInitialURL = async () => {
+      const initialURL = await Linking.getInitialURL();
+      if (initialURL) {
+        handleDeepLink(initialURL);
+      }
+    };
+    getInitialURL();
+    const subscription = Linking.addEventListener('url', (e) => {
+      handleDeepLink(e.url);
+    });
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
+  const handleDeepLink = (url: string) => {
+    const { path, queryParams } = Linking.parse(url);
+    const cardId = path.split('/')[2];
+    console.log(`cardId: ${cardId}`);
+  };
 
   return (
     <QueryClientProvider client={queryClient}>
